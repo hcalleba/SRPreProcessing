1c15bfc02c48c88c95372c61b965f1d2b560498c
hcalleba <hugo.callebaut.sjb@gmail.com>
2023-07-10 12:54:01 +0200

    Update heatmap.py

--- data/unaryGraphs/heatmap.py
+++ data/unaryGraphs/heatmap.py
@@ -4,6 +4,12 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable, axes_size
 
 NBSR = "2"
 
+def get_last_non_zero_index(l):
+    for i in range(len(l)-1, -1, -1):
+        if l[i] != 0:
+            return i+1
+    return 0
+
 def on_resize(event):
         plt.tight_layout()
         plt.gcf().canvas.draw()
@@ -20,6 +26,20 @@ for i in mat2:
     for j in i:
         newmat[-1].append(j/somme)
 
+li = get_last_non_zero_index(newmat[0][:-1])
+
+bar = plt.bar(range(len(newmat[0][:li])), newmat[0][:li], color="r", label="1")
+i = 0
+for rect in bar:
+    if (mat2[0][i] != 0):
+        height = rect.get_height()
+        plt.text(rect.get_x() + rect.get_width() / 2.0, height, mat2[0][i], ha='center', va='bottom', rotation = 90)#, rotation_mode = 'anchor')
+    i+=1
+
+#plt.legend()
+
+
+"""
 plt.imshow(newmat, cmap="Greys", interpolation='None', norm=matplotlib.colors.LogNorm())
 
 for i in range(len(mat2)):
@@ -51,6 +71,6 @@ pad = axes_size.Fraction(pad_fraction, width)
 cax = divider.append_axes("right", size=width, pad=pad)
 plt.colorbar(im, cax=cax)
 
-#cid = plt.gcf().canvas.mpl_connect('resize_event', on_resize)
+#cid = plt.gcf().canvas.mpl_connect('resize_event', on_resize)"""
 
 plt.show()
