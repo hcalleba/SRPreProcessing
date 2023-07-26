d71fceaabe07e0cecb9330f416c838c9cbfa0234
hcalleba <hcalleba@ulb.ac.be>
2023-07-26 11:59:43 +0200

    cleanup

---
+++ BenchmarkResults/topology-size.py
@@ -0,0 +1,24 @@
+import os
+
+directory = "../data/2016TopologyZooUCL_inverseCapacity"
+def giveTopoNames(dir):
+    for file in os.listdir(dir):
+        if file.endswith(".graph"):
+            yield file[:-6]
+
+lst = []
+for topology in giveTopoNames(directory):
+    with open(directory + "/" + topology + ".graph", "r") as f:
+        nbNodes = f.readline().split()[-1]
+        while (True):
+            a = f.readline()
+            if (a.startswith("EDGES")):
+                break
+        nbEdges = a.split()[-1]
+        lst.append((topology, nbNodes, nbEdges))
+
+lst.sort(key=lambda x: (int(x[1]), int(x[2])))
+
+for i in lst[:50]:
+    print(i[0])
+
